611714a5d4a38b817356679446d1ca2585962772
Josh Owens <jowens@dealerinspire.com>
2018-04-04 16:09:57 -0500

    Adding update-repo script to pull any changes made to various dot files on system so they can be commited to source control

---
+++ scripts/update-repo.sh
@@ -0,0 +1,32 @@
+# Updates the dotfiles repo with any changes that may have been made
+# to the dotfiles once they were moved into their proper directories 
+# on a given machine
+
+
+read -p "[96mThis is going to wipe any uncommited changes in the dotfiles repo, are you sure? " -n 1 -r
+echo    # moved to a new line
+if [[ $REPLY =~ ^[Yy]$  ]]
+then
+  # Get the updated version on .hyper.js 
+  cp ~/.hyper.js ../.hyper.js
+  echo "[92m.hyper.js updated"
+
+  # Get the updated version of init.vim 
+  cp ~/.config/nvim/init.vim ../init.vim
+  echo "[92minit.vim updated"
+
+  # Get updated version of .tmux.conf 
+  cp ~/.tmux.conf ../.tmux.conf
+  echo "[92m.tmux.conf updated"
+
+  # Get updated version of .zshrc
+  cp ~/.zshrc ../.zshrc
+  echo "[92m.zshrc updated"
+
+  # Get updated version of .vimrc
+  cp ~/.vimrc ../.vimrc
+  echo "[92m.vimrc updated"
+
+else
+  echo "[91mDotfiles NOT updated"
+fi
