1718448bed9379ce7bb81ef48ad0589961d57b8f
jowens <jowens@dealerinspire.com>
2019-10-28 09:11:32 -0500

    Updating linux file with install helper function

--- linux.js
+++ linux.js
@@ -1,17 +1,25 @@
+const { install } = require('./utils.js');
 
+/**
+ * Runs linux specific installs
+ *
+ * return {Promise}
+ */
 async function linux() {
   neovim()
 }
 
+/**
+ * Installs neovim via the neovim ppa
+ *
+ * @return {Promise}
+ */
 async function neovim() {
-  console.log('Installing NeoVim...')
-  const { stdout, stderr } = await exec('sudo add-apt-repository ppa:neovim-ppa/stable -y && sudo apt-get update && sudo apt-get install neovim -y');
-
-  if (stderr) {
-    console.log('ERROR: ', stderr);
-  }
-  console.log(stdout)
-  console.log('NeoVim Finished!')
+  await install({
+    installingMessage: 'Instaling neovim...',
+    command: 'sudo add-apt-repository ppa:neovim-ppa/stable -y && sudo apt-get update && sudo apt-get install neovim -y',
+    successMessage: 'neovim installed!',
+  });
 }
 
 module.export = linux;
